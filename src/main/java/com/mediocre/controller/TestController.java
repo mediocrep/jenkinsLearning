0565c5f41c21b180a9bae60b38bcc84b24b7df28
witt <zhangwei3583@126.com>
2021-11-15 10:46:29 +0800

    testcontroller

--- src/main/java/com/mediocre/controller/TestController.java
+++ src/main/java/com/mediocre/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
+        System.out.println("test log: hello world.");
         return "Hello World!";
     }
 }
