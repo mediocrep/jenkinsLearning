7d5aa5a801315e39d55dbfa7641a40b028bbf383
witt <zhangwei3583@126.com>
2021-11-15 10:21:06 +0800

    add test module update1

--- src/test/java/com/mediocre/ApplicationTests.java
+++ src/test/java/com/mediocre/ApplicationTests.java
@@ -1,12 +1,12 @@
 package com.mediocre;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+//import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
-@ExtendWith(SpringExtension.class)
+//@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ApplicationTests {
 
